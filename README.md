b066cf26bddabb2f8ed2b957101e860e038f15c4
BryanKelly13 <bk20bu@fsu.edu>
2022-08-31 12:10:52 -0400

    updated README

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # angular_distributions
 Cross-section and angular distirbutions calculations at the Super Enge Split-Pole Spectrograph at FSU.
+
+Run the code simply by running 'python3 angular_distribution_calculations.py'
+
+The code expects BCI information stored in text files (currently supports up to 2 different isotopes, will be worked on to support any amount)
+	BCI info should be stored as:   BCI hits     BCI Scale(nA)   --> in a two column list 
+				i.e	  10000		   10
+It also looks for directories that store the .txt files that hold the volume list for each peak of interest
+
+The code loops through and calculates cross sections using the volume lists stored and will plot an angular distribution across all angles
+	Two things: 1) if you do not have a volume measurement at an angle, leave a placeholder blank in at its angle
+		    2) Right now it covers angles (15-60) in steps of 5 degrees, you can change the desired angles in the first line of the main() function
+
+Angular Distributions will be shown in a grid of plots from smallest energy-->largest energy.
--- angular_distribution_calculations.py
+++ angular_distribution_calculations.py
@@ -1,25 +1,23 @@
-from cProfile import label
-from cmath import log
-import os
-import matplotlib
-import matplotlib.pyplot as plt
-import numpy
 
-# some useful constants
+import os
+import numpy as np
+import time
+import matplotlib.pyplot as plt
+import math
+
 
 AVAGADRO_NUM = 6.023E23
 CM_TO_BARN = 1.0E-24 # converts cm^2 to barns
 SAMPLING_RATE = 100.0 # Hz
 ELEMENTARY_CHARGE = 1.60E-19 # in C
 D_OMEGA_= 0.00463 # slit settings in steradians
-Z = 1 # Number of protons in beam 
+Z = 1 # Number of protons in beam
 
 MOLAR_MASS_49TI = 4.89E7 #ug/mol
 TARGET_49TI = 413 # in micrograms/cm^2
 
 MOLAR_MASS_47TI = 4.70E7 #ug/mol
 TARGET_47TI = 441 # in micrograms/cm^2
-
 
 def BCI_info_handler():
     BCI_hits_47Ti = []
@@ -33,104 +31,186 @@ def BCI_info_handler():
             hits, scale = line.split()
             BCI_hits_47Ti.append(float(hits))
             BCI_scale_47Ti.append(float(scale))
-    
+   
     with open("BCI_info_49Ti.txt") as f:
         stripped = [s.strip() for s in f]
         for line in stripped:
             hits, scale = line.split()
             BCI_hits_49Ti.append(float(hits))
             BCI_scale_49Ti.append(float(scale))
-    
     return BCI_hits_47Ti, BCI_scale_47Ti, BCI_hits_49Ti, BCI_scale_49Ti
+
 
 def volume_file_reader(dir, file):
     vol = []
     with open(dir + '/' + file) as f:
             stripped = [s.strip() for s in f]
             for line in stripped:
-                vol.append(float(line))
+                if line == '':
+                    vol.append(0.0)
+                else:
+                    vol.append(float(line))
     return vol
 
-
-def cross_section_47Ti(BCI_hits_47Ti, BCI_scale_47Ti, volume_list): #func used to convert mass in ug/cm --> 1/barn
+def cross_section_calculation(BCI_hits, BCI_scale, volume_list, isotope): #func used to convert mass in ug/cm --> 1/barn
     rho_t_47Ti = (TARGET_47TI * CM_TO_BARN * AVAGADRO_NUM)/(MOLAR_MASS_47TI)
-
-    j=0
-    cross_section_vals = []
-    for i in BCI_hits_47Ti:
-        Q_beam = (i * 1E-9 * BCI_scale_47Ti[j])/(SAMPLING_RATE)
-        N_beam = Q_beam / ELEMENTARY_CHARGE
-        dsigma_domega = (volume_list[j] * 1000)/(N_beam * rho_t_47Ti * D_OMEGA_)  # cross-sec in mb/sr
-        cross_section_vals.append(dsigma_domega)
-        j +=1
-    return cross_section_vals
-
-
-def cross_section_49Ti(BCI_hits_49Ti, BCI_scale_49Ti, volume_list):
     rho_t_49Ti = (TARGET_49TI * CM_TO_BARN * AVAGADRO_NUM)/(MOLAR_MASS_49TI)
 
     j=0
     cross_section_vals = []
-    for i in BCI_hits_49Ti:
-        Q_beam = (i * 1E-9 * BCI_scale_49Ti[j])/(SAMPLING_RATE)
+    for i in BCI_hits:
+        Q_beam = (i * 1E-9 * BCI_scale[j])/(SAMPLING_RATE)
         N_beam = Q_beam / ELEMENTARY_CHARGE
-        dsigma_domega = (volume_list[j] * 1000)/(N_beam * rho_t_49Ti * D_OMEGA_)  # cross-sec in mb/sr
+        if isotope == 47:
+            dsigma_domega = (volume_list[j] * 1000)/(N_beam * rho_t_47Ti * D_OMEGA_)  # cross-sec in mb/sr
+        elif isotope == 49:
+            dsigma_domega = (volume_list[j] * 1000)/(N_beam * rho_t_49Ti * D_OMEGA_)  # cross-sec in mb/sr
         cross_section_vals.append(dsigma_domega)
         j +=1
     return cross_section_vals
 
 
-def plot_cross_section(lab_angles, x_sec, energy_beforesplit):
-    name_list = energy_beforesplit.split('_')
-    energy = ' '.join(name_list)
-    print(energy)
-    plt.scatter(lab_angles, x_sec, label=energy + ' data')
-    plt.yscale("log")
+def plotting(dir, lab_angles, BCI_scale, BCI_hits, num_files, isotope):
+    #plot info stuff
+    figure_size = (15,15)
 
-    miny=0.01
-    maxy=1
-    for val in x_sec:
-        if val < miny:
-            miny = 0.001
-        if val > maxy:
-            maxy = 10
-    plt.ylim(miny, maxy)
+    if (num_files <= 12):
+        colz=3
+        row = math.ceil(num_files/3)
+        fig, ax = plt.subplots(row, colz, squeeze=False, figsize=figure_size)
+        fig.supxlabel(r'Lab Angle [$\Theta_{lab}$]')
+        fig.supylabel(r'Cross-Section [$\frac{mb}{sr}$]')
+    else:
+        # create 2 diff subplots, to be implemented later
+        row=4
+        colz=4
+        sorted_files = sorted(os.listdir(dir))
+        file_sets = [sorted_files[x:x+16] for x in range(0, len(sorted_files), 16)]
+        i = 0
+        for _ in file_sets:
+            curr_row = 0
+            curr_col = 0
+            fig, ax = plt.subplots(colz, row, squeeze=False, figsize=figure_size)
+            fig.supxlabel(r'Lab Angle [$\Theta_{lab}$]')
+            fig.supylabel(r'Cross-Section [$\frac{mb}{sr}$]')
+            for file in file_sets[i]:
+                miny=0.01
+                maxy=1
+                vol_list = volume_file_reader(dir, file)
+                fname = file.split('.')[0]
+                fname_list = fname.split('_')
+                plot_name = ' '.join(fname_list)
+                cross_sec = cross_section_calculation(BCI_hits, BCI_scale, vol_list, isotope)
+                if (curr_row<row and curr_col<colz):
+                    ax[curr_row][curr_col].scatter(lab_angles, cross_sec, label=plot_name)
+                    ax[curr_row][curr_col].minorticks_on()
+                    ax[curr_row][curr_col].legend()
+                    ax[curr_row][curr_col].set_yscale("log")
 
-    plt.minorticks_on()
+                    for val in cross_sec:
+                        if val < miny:
+                            miny = 0.001
+                        if val > maxy:
+                            maxy = 10
 
-    plt.legend(loc='upper right')
-    plt.ylabel(r'Cross-Section [$\frac{mb}{sr}$]')
-    plt.xlabel(r'Lab Angle [$\Theta_{lab}$]')
-    plt.show()
+                    ax[curr_row][curr_col].set_ylim(miny, maxy)
+                    curr_col+=1
+                elif(curr_col == colz):
+                    curr_row+=1
+                    curr_col = 0
+                    ax[curr_row][curr_col].scatter(lab_angles, cross_sec, label=plot_name)
+                    ax[curr_row][curr_col].minorticks_on()
+                    ax[curr_row][curr_col].legend()
+                    ax[curr_row][curr_col].set_yscale("log")
+
+                    for val in cross_sec:
+                        if val < miny:
+                            miny = 0.001
+                        if val > maxy:
+                            maxy = 10
+            
+                    ax[curr_row][curr_col].set_ylim(miny, maxy)
+                    curr_col+=1
+            plt.show()
+            i+=1
+        return
     
+    # Hits here after if statement is true, plots the < 12 file case
+    curr_row = 0
+    curr_col = 0
+    sorted_files = sorted(os.listdir(dir))
+    for file in sorted_files:
+        miny=0.01
+        maxy=1
+        vol_list = volume_file_reader(dir, file)
+        fname = file.split('.')[0]
+        fname_list = fname.split('_')
+        plot_name = ' '.join(fname_list)
+        cross_sec = cross_section_calculation(BCI_hits, BCI_scale, vol_list, isotope)
+        if (curr_row<row and curr_col<colz):
+            ax[curr_row][curr_col].scatter(lab_angles, cross_sec, label=plot_name)
+            ax[curr_row][curr_col].minorticks_on()
+            ax[curr_row][curr_col].legend()
+            ax[curr_row][curr_col].set_yscale("log")
 
+            for val in cross_sec:
+                if val < miny:
+                    miny = 0.001
+                if val > maxy:
+                    maxy = 10
 
+            ax[curr_row][curr_col].set_ylim(miny, maxy)
+            curr_col+=1
+        elif(curr_col == colz):
+            curr_row+=1
+            curr_col = 0
+            ax[curr_row][curr_col].scatter(lab_angles, cross_sec, label=plot_name)
+            ax[curr_row][curr_col].minorticks_on()
+            ax[curr_row][curr_col].legend()
+            ax[curr_row][curr_col].set_yscale("log")
+
+            for val in cross_sec:
+                if val < miny:
+                    miny = 0.001
+                if val > maxy:
+                    maxy = 10
+       
+            ax[curr_row][curr_col].set_ylim(miny, maxy)
+            curr_col+=1
+    plt.show()
+
+    
 def main():
+
     lab_angles = [15, 20, 25, 30, 35, 40, 45, 50, 55, 60]
     BCI_hits_47Ti, BCI_scale_47Ti, BCI_hits_49Ti, BCI_scale_49Ti = BCI_info_handler()
+
+    # 47Ti Plots & Calculations
     dir = os.getcwd()
     dir_47Ti = dir + '/47Ti_peaks'
+    num_files = len(os.listdir(dir_47Ti))
+    isotope_47Ti = 47
+    plotting(dir_47Ti, lab_angles, BCI_scale_47Ti, BCI_hits_47Ti, num_files, isotope_47Ti)
+
+    # 49Ti Plots & Calculations
+    dir = os.getcwd()
     dir_49Ti = dir + '/49Ti_peaks'
-    for file in os.listdir(dir_47Ti):
-        '''
-        To find # of subplots, just use len(files in folder)
-        Can get fancy and use modulo % operator to chuck into grids, max prolly 4x4, in energy order
-        The subplots can be spawned using subplots(i,i)
-        '''
-        vol_list = volume_file_reader(dir_47Ti, file)
-        energy = file.split('.')[0]
-        x_sec = cross_section_47Ti(BCI_hits_47Ti, BCI_scale_47Ti, vol_list)
-        plot_cross_section(lab_angles, x_sec, energy)
-
-
-    for file in os.listdir(dir_49Ti):
-        vol_list = volume_file_reader(dir_49Ti, file)
-        energy = file.split('.')[0]
-        x_sec = cross_section_47Ti(BCI_hits_49Ti, BCI_scale_49Ti, vol_list)
-        plot_cross_section(lab_angles, x_sec, energy)
-
-
+    num_files = len(os.listdir(dir_49Ti))
+    isotope_49Ti = 49
+    plotting(dir_49Ti, lab_angles, BCI_scale_49Ti, BCI_hits_49Ti, num_files, isotope_49Ti)
 
 
 if __name__ == "__main__":
     main()
+
+
+'''
+Old Conditinal Statement that was used for file length < 16, but now I just use the indexing through 
+one file set instead, keeping just in case
+'''
+# elif (num_files <=16 ):
+    #     row=4
+    #     colz =math.ceil(num_files/4)
+    #     fig, ax = plt.subplots(colz, row, squeeze=False, figsize=figure_size)
+    #     fig.supxlabel(r'Lab Angle [$\Theta_{lab}$]')
+    #     fig.supylabel(r'Cross-Section [$\frac{mb}{sr}$]')
